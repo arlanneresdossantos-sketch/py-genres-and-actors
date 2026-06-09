f751ef36f33faaf649e1cc6706375f3469097c35
Arlan Neres <arlanneresdossantos@gmail.com>
2026-06-09 17:31:21 -0300

    solution

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import init_django_orm  # noqa: F401
+# main.py
+from db.models import Genre, Actor
 
-from django.db.models import QuerySet
 
+def main() -> str:
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Ação")
+    Genre.objects.create(name="Drama")
 
-def main() -> QuerySet:
-    pass
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+
+    Genre.objects.filter(name="Drama").update(name="Drama")
+    Actor.objects.filter(first_name="George",
+                         last_name="Klooney").update(last_name="Clooney")
+
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
+
+    Genre.objects.filter(name="Ação").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(
+        last_name="Smith").order_by("first_name").values_list(
+        "first_name", "last_name")
