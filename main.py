1d794a81a60e2a7feb348ee1f1d2bc622398e6c6
Arlan Neres <arlanneresdossantos@gmail.com>
2026-06-11 16:26:06 -0300

    solution 16

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    # 1. Create (Usando estritamente listas de tuplas e desempacotamento no for)
     genres_to_create = [
         ("Western",),
         ("Action",),
@@ -27,8 +26,10 @@ def main() -> QuerySet:
 
     # 2. Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(
+        last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu", last_name="Reeves")
 
     # 3. Delete
     Genre.objects.filter(name="Action").delete()
