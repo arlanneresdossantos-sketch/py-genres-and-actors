4791b0ef134266b9a0581bb2eac5114136ec396f
Arlan Neres <arlanneresdossantos@gmail.com>
2026-06-11 15:24:02 -0300

    solution2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# main.py
+import init_django_orm  # noqa: F401
 from db.models import Genre, Actor
 
 
-def main() -> str:
+def main():
+    # 1. Create
     Genre.objects.create(name="Western")
-    Genre.objects.create(name="Ação")
-    Genre.objects.create(name="Drama")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
 
     Actor.objects.create(first_name="George", last_name="Klooney")
     Actor.objects.create(first_name="Kianu", last_name="Reaves")
@@ -14,18 +15,15 @@ def main() -> str:
     Actor.objects.create(first_name="Jaden", last_name="Smith")
     Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
-    Genre.objects.filter(name="Drama").update(name="Drama")
-    Actor.objects.filter(first_name="George",
-                         last_name="Klooney").update(last_name="Clooney")
+    # 2. Update
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
 
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
-        first_name="Keanu",
-        last_name="Reeves"
-    )
-
-    Genre.objects.filter(name="Ação").delete()
+    # 3. Delete
+    Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    return Actor.objects.filter(
-        last_name="Smith").order_by("first_name").values_list(
-        "first_name", "last_name")
+    # 4. Return filtered and ordered QuerySet
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
